fd737165cc500a6610d7429040e0e290663f7cde
Marco Marques <marcohenriquejava@gmail.com>
2016-10-05 19:22:06 -0300

    Visitante

--- src/main/java/br/com/thiago/condoApp/modelo/Visitante.java
+++ src/main/java/br/com/thiago/condoApp/modelo/Visitante.java
@@ -10,8 +10,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 @Entity
 public class Visitante implements Serializable {
 	
@@ -30,7 +28,6 @@ private static final long serialVersionUID = 1L;
 	
 	@OneToOne
 	@JoinColumn(name = "apartamento_id")
-	@JsonIgnore
 	private Apartamento apartamento;
 
 
